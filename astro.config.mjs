4acaf7dcac5a9a70cf63c1cb3f50ab3ba9c92598
Irenio Johan Aponte Giron <aluno.johan@gmail.com>
2025-09-03 19:09:20 -0400

    Atualiza título e descrição da documentação do SUAP e ajusta o conteúdo do índice

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,17 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Manual do SUAP',
+			description: 'Documentação do Sistema Unificado de Administração Pública - SUAP - IFRR',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }, 
+					 { icon: 'instagram', label: 'Instagram', href: 'https://www.instagram.com/caads.ifrr/' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Reference', slug: 'reference/example' },
 					],
 				},
 				{
